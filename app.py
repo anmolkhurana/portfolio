92f254b1a92e32b21558ce54cdc27700278b56a6
Anmol Khurana <anmolkhurana@Anmols-MacBook-Pro-2.local>
2021-05-16 20:44:45 +0530

    V1.0.0 Release

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Request
+from flask.globals import request
 app = Flask(__name__)
 
 @app.route('/')
-def land():
+def landing():
     return render_template('index.html')
+
+@app.route('/sendemail', methods=['POST'])
+def sendEmail():
+    print(request.form['name'])
+    return 'Email Sent!!! He will get back shortly'
 
 if __name__ == 'main':
     app.run(debug=True)
